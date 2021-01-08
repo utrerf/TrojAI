3f2d7c6b4915eaf71402c4734a547515a3452bd4
Geoffrey Negiar <geoffrey_negiar@berkeley.edu>
2021-01-08 13:29:45 -0800

    group lasso constants

--- trojan_detector.py
+++ trojan_detector.py
@@ -51,8 +51,8 @@ def trojan_detector(model_filepath, result_filepath, scratch_dirpath,
     constraints_to_eps = {
        'inf' : [.07, .15, .5, .2],
        '2'   : [.5, 2., 4., 8., 10., 16., 20],
-       'groupLasso': [.1, .5, 1.5, 2., 4., 10.],
-       'tracenorm': [.1, .5, 1., 5.]
+       'tracenorm': [.1, .5, 1., 5.],
+       'groupLasso': np.linspace(-5, -1, num=10)
     }
     for constraint, eps_list in constraints_to_eps.items():
         for eps in eps_list:
